d87701c93ac9d7b916560f6035ec7c01b5f92834
hxgned <dengxionghui@hotmail.com>
2021-05-02 10:22:02 +0800

    src: frontend: fix linking with libcrail for gg main program 1. fix compile order 2. add link entry for gg frontend

--- src/net/crail.cc
+++ src/net/crail.cc
@@ -1,10 +1,12 @@
 #include "net/crail.hh"
 
+// TODO: finish this upload files interface
 void Crail::upload_files( const std::vector<storage::PutRequest> & upload_requests,
                      const std::function<void( const storage::PutRequest & )> & success_callback) {
 
 }
 
+// TODO: finish this download files interface
 void Crail::download_files(const std::vector<storage::GetRequest> & download_requests,
                        const std::function<void( const storage::GetRequest & )> & success_callback) {
 
