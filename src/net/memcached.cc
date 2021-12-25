78c91ac2437f1cbbc4683e177930ae1a4324dcbc
hxgned <dengxionghui@hotmail.com>
2021-12-24 18:16:27 -0800

    memcached.cc: implement method upload_files and download_files

--- src/net/memcached.cc
+++ src/net/memcached.cc
@@ -16,14 +16,96 @@
 using namespace std;
 using namespace logger;
 
+// callback is useless at all
 void Memcached::upload_files(const vector<storage::PutRequest> &upload_requests, const function<void( const storage::PutRequest & )> &success_callback) {
-    log_debug("upload_files begin");
-    log_debug("upload_files end");
+    log_debug("method upload_files begin");
+
+    log_debug("size of upload_requests -> [%d]", upload_requests.size());
+
+    log_debug("begin construct MemcachedClient obj");
+    MemcachedClient client(this->_config);
+    log_debug("end construct MemcachedClient obj");
+
+    log_debug("begin connect to memcached server");
+    if (client.Connect() < 0) {
+        throw runtime_error( "error connecting to memcached server" );
+    }
+    log_debug("end connect to memcached server");
+
+    // upload all files to storage
+    for (size_t i = 0; i < upload_requests.size(); i++) {
+        log_debug("begin process upload_requests[%d]", i);
+
+        const string & filename = upload_requests.at(i).filename.string();
+        const string & object_key = upload_requests.at(i).object_key;
+
+        log_debug("filename -> [%s]", filename.c_str());
+        log_debug("object_key -> [%s]",  object_key.c_str());
+
+        string contents;
+
+        FileDescriptor file { CheckSystemCall( "open " + filename, open( filename.c_str(), O_RDONLY ) ) };
+        while ( not file.eof() ) { contents.append( file.read() ); }
+        file.close();
+
+        log_debug("begin set key [%s] for file [%s]", object_key.c_str(), filename.c_str());
+        if (client.Set(object_key, contents) < 0) {
+            log_error("failed to set key [%s] to memcached", object_key.c_str());
+            throw runtime_error("failed to set key");
+        }
+        log_debug("end set key [%s] for file [%s]", object_key.c_str(), filename.c_str());
+
+        success_callback(upload_requests[i]);
+
+        log_debug("end process upload_requests[%d]", i);
+    }
+
+    log_debug("method upload_files end");
 }
 
+// callback is useless at all
 void Memcached::download_files(const std::vector<storage::GetRequest> &download_requests, const std::function<void( const storage::GetRequest & )> &success_callback) {
-    log_debug("download_files begin");
-    log_debug("download_files end");
+    log_debug("method download_files begin");
+
+    log_debug("size of download_requests -> [%d]", download_requests.size());
+
+    log_debug("begin construct MemcachedClient obj");
+    MemcachedClient client(this->_config);
+    log_debug("end construct MemcachedClient obj");
+
+    log_debug("begin connect to memcached server");
+    if (client.Connect() < 0) {
+        log_error("error connecting to memcached server");
+        throw runtime_error( "error connecting to memcached server" );
+    }
+    log_debug("end connect to memcached server");
+
+    for (size_t i = 0; i < download_requests.size(); i++) {
+        log_debug("begin process download_requests[%d]", i);
+
+        const string & filename = download_requests.at(i).filename.string();
+        const string & object_key = download_requests.at(i).object_key;
+
+        log_debug("filename -> [%s]", filename.c_str());
+        log_debug("object_key -> [%s]",  object_key.c_str());
+
+        string str_data;
+
+        if (client.Get(object_key, str_data) < 0) {
+            log_error("failed to get key [%s] to memcached", object_key.c_str());
+            throw runtime_error("failed to get key");
+        }
+
+        log_info("begin create file [%s] for key [%s]", filename.c_str(), object_key.c_str());
+        roost::atomic_create(str_data, filename, download_requests[i].mode.initialized(), download_requests[i].mode.get_or( 0 ));
+        log_info("end create file [%s] for key [%s]", filename.c_str(), object_key.c_str());
+
+        success_callback( download_requests[i]);
+
+        log_debug("end process download_requests[%d]", i);
+    }
+
+    log_debug("method download_files end");
 }
 
 MemcachedClient::MemcachedClient(MemcachedClientConfig &config)  : _mem_connect(nullptr), _addr(""), _port(0) {
